bf351f1b2a8418d2540807288ccb712d6e319ce1
Alex Nik <catalog.pri@gmail.com>
2021-03-25 17:46:28 +0200

    Added Error js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import SearchBox from "../components/SearchBox";
-import CardList from "../components/CardList";
-import "./App.css";
 import Scroll from "../components/Scroll";
+import CardList from "../components/CardList";
+import ErrorBoundry from "../components/ErrorBoundry";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -41,7 +42,9 @@ class App extends Component {
         <h1 className="f1">Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
